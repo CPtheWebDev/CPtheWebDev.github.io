e5ec3a0450d7bf2e7ca83e7613f063cc0d894367
Chris <cpatton302@gmail.com>
2020-08-11 15:18:45 -0400

    fixed code validation errors

--- js/main.js
+++ js/main.js
@@ -9,29 +9,39 @@ const projects = [
   `<div class="project flex fade">
     <h3>Resgistration Form</h3>
     <p>This project displays a responsive form for users signing up for a newsletter. It uses the different form types available in HTML.</p>
-    <a href="https://cpthewebdev.github.io/techdegree-project-3/"><img src="images/Projects/online-web-form.png"></a>
+    <a href="https://cpthewebdev.github.io/techdegree-project-3/">
+      <img src="images/Projects/online-web-form.png" alt="Online Web Form">
+    </a>
   </div>`,
   `<div class="project flex fade">
     <h3>Web Style Guide</h3>
     <p>This project uses the Sass extension language to create a style guide that can be used to prototype websites.</p>
-    <a href="https://cpthewebdev.github.io/techdegree-project-4/"><img src="images/Projects/web-style-guide.png"></a>
+    <a href="https://cpthewebdev.github.io/techdegree-project-4/">
+      <img src="images/Projects/web-style-guide.png" alt="Web Style Guide">
+    </a>
   </div>`,
   `<div class="project flex fade">
     <h3>Game Show App</h3>
     <p>This project is an interactive game using Javascript where a users attempts to guess a random phrase with a limited amount of attmepts.</p>
-    <a href="https://cpthewebdev.github.io/techdegree-project-6/"><img src="images/Projects/game-show-app.png"></a>
+    <a href="https://cpthewebdev.github.io/techdegree-project-6/">
+      <img src="images/Projects/game-show-app.png" alt="Game Show App">
+    </a>
   </div>`,
   `<div class="project flex fade">
     <h3>WebApp Dashboard</h3>
     <p>This project displays the landing page for a WebApp Dashboard and uses a Javascript chart library.</p>
-    <a href="https://cpthewebdev.github.io/techdegree-project-7/"><img src="images/Projects/web-app-dashboard.png"></a>
+    <a href="https://cpthewebdev.github.io/techdegree-project-7/">
+      <img src="images/Projects/web-app-dashboard.png" alt="Web App Dashboard">
+    </a>
   </div>`,
   `<div class="project flex fade">
     <h3>Employee directory</h3>
     <p>Using fetch this project grabs information for 12 random employees from an API and diplays the employess in a directory.</p>
-    <a href="https://cpthewebdev.github.io/techdegree-project-8/"><img src="images/Projects/employee-directory.png"></a>
+    <a href="https://cpthewebdev.github.io/techdegree-project-8/">
+      <img src="images/Projects/employee-directory.png" alt="Employee Directory">
+    </a>
   </div>`
-]
+];
 
 // displays a list upon clicking the menu button
 window.addEventListener('click', (event) => {
@@ -66,7 +76,7 @@ function showSlides() {
 	} 
 
   // removes active class from all triggers
-  for (i = 0; i < slideTriggers.length; i++) { 
+  for (let i = 0; i < slideTriggers.length; i++) { 
 		slideTriggers[i].className = slideTriggers[i].className.replace(" active", ""); 
 	} 
 
